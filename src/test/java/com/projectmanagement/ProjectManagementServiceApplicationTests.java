ae74d9f6e0e88b54ce02b970b81a0c2f9b397e8d
vivek-kakumanu <58516046+vivek-kakumanu@users.noreply.github.com>
2019-12-25 17:47:42 +0530

    Unit Test
    Unit Test

--- src/test/java/com/projectmanagement/ProjectManagementServiceApplicationTests.java
+++ src/test/java/com/projectmanagement/ProjectManagementServiceApplicationTests.java
@@ -1,11 +1,21 @@
 package com.projectmanagement;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.projectmanagement.dao.TaskDaoImpl;
 import com.projectmanagement.restservice.TaskManagementRestfulService;
+import com.projectmanagement.model.ParentTask;
+import com.projectmanagement.model.Task;
 
 
 @SpringBootTest
@@ -38,4 +48,23 @@ class ProjectManagementServiceApplicationTests {
         
     }
 */
+    @Test
+    public void testGetTasks() 
+    {
+        // given
+    	Task task1 = new Task( new ParentTask("parentTestTask"),null, "Testtaskname",20,new Date(), new Date(), false);
+    	Task task2 = new Task( new ParentTask("parentTestTask1"),null, "Testtaskname1",20,new Date(), new Date(), false);
+        ArrayList<Task> list = new ArrayList<Task>();
+        list.add(task1);
+        list.add(task2);
+ 
+        when(taskDao.getTasks()).thenReturn(list);
+ 
+        // when
+        List<Task> result = taskDao.getTasks();
+ 
+        // then
+        assertEquals(result.size(), list.size());
+        assertEquals(result.get(0).getTaskName(), list.get(0).getTaskName());
+    }
 }
